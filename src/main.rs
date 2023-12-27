de00615ec27e71567cec7eb0a3b49ce71a105974
TieWay59 <tieway59@foxmail.com>
2023-12-28 02:11:02 +0800

    整合 graphql_client 功能，分离查询文件和代码（重大变化）

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use reqwest::{
-    blocking, header,
-    header::{HeaderMap, HeaderValue},
-};
-use serde_json::json;
-
+use graphql_client::GraphQLQuery;
+use reqwest::{blocking, header, header::HeaderMap};
 use std::io::Write;
+
+use crate::get_repository_discussions::Variables;
+use graphql_client::reqwest::post_graphql_blocking;
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 struct Config {
     token: String,
     user_agent: String,
 }
+
+// TODO: 暂时不知道为什么，但是 https://github.com/graphql-rust/graphql-client/blob/main/examples/github/examples/github.rs 案例中这样写。
+#[allow(clippy::upper_case_acronyms)]
+type DateTime = String;
+
+#[allow(clippy::upper_case_acronyms)]
+type URI = String;
+
+// The paths are relative to the directory where your `Cargo.toml` is located.
+// Both json and the GraphQL schema language are supported as sources for the schema
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schemas/schema.docs.graphql",
+    query_path = "get_repository_discussions.graphql",
+    response_derives = "Debug, Serialize, Deserialize"
+)]
+pub struct GetRepositoryDiscussions;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
@@ -20,6 +36,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let auth = format!("bearer {token}");
 
+    // TODO: 这里可以 collect
     let headers: HeaderMap = HeaderMap::from_iter([
         (header::AUTHORIZATION, auth.parse()?),
         (header::USER_AGENT, user_agent.parse()?),
@@ -31,52 +48,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .https_only(true)
         .build()?;
 
-    // TODO: 这里需要模板化
-    let query_body = r#"
-{
-  repository(owner: "mermaid-js", name: "mermaid") {
-    discussions(
-      after: "Y3Vyc29yOnYyOpK5MjAyMy0wNi0wM1QwMjowOToyMSswODowMM4AUD8C"
-      first: 3
-      answered: true
-      orderBy: { field: CREATED_AT, direction: DESC }
-    ) {
-      pageInfo { 
-        endCursor
-      }
-      nodes {
-        title
-        body
-        url
-        answer {
-          body
-          publishedAt
-          reactions {
-            totalCount
-          }
-        }
-      }
-    }
-  }
-}"#;
+    let variables = Variables {
+        repo_owner: "mermaid-js".to_string(),
+        repo_name: "mermaid".to_string(),
+        // TODO 还不确定这个缺省该怎么处理
+        query_cursor: Some("Y3Vyc29yOnYyOpK5MjAyMy0wNi0wM1QwMjowOToyMSswODowMM4AUD8C".into()),
+        query_window: Some(3),
+    };
 
-    let payload = json!({
-        "query": query_body
-    });
+    let response_body = post_graphql_blocking::<GetRepositoryDiscussions, _>(
+        &client,
+        "https://api.github.com/graphql",
+        variables,
+    )?;
 
-    let resp_text = client
-        .post("https://api.github.com/graphql")
-        .bearer_auth(token)
-        .json(&payload)
-        .send()?
-        .text()?;
+    let response_data: get_repository_discussions::ResponseData =
+        response_body.data.expect("missing response data");
 
-    let parsed_json: serde_json::Value = serde_json::from_str(&resp_text)?;
-
-    // println!("{:#?}", parsed_json);
+    let parsed_json = serde_json::to_string(&response_data)?;
 
     // 把 parsed_json 这个文件导出到 output.json 当中
-    let mut file = std::fs::File::create("output.json")?;
+    let mut file = std::fs::File::create("output2.json")?;
     file.write_all(parsed_json.to_string().as_bytes())?;
 
     println!("finished");
