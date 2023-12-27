94d14832c5f76639decf99261c43f0f8d471d4d4
TieWay59 <tieway59@foxmail.com>
2023-12-28 03:14:05 +0800

    拆分代码，实现导出函数

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use graphql_client::GraphQLQuery;
-use reqwest::{blocking, header, header::HeaderMap};
-use std::io::Write;
+mod query;
 
-use crate::get_repository_discussions::Variables;
-use graphql_client::reqwest::post_graphql_blocking;
+use reqwest::{blocking, header};
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 struct Config {
@@ -11,67 +8,46 @@ struct Config {
     user_agent: String,
 }
 
-// TODO: 暂时不知道为什么，但是 https://github.com/graphql-rust/graphql-client/blob/main/examples/github/examples/github.rs 案例中这样写。
-#[allow(clippy::upper_case_acronyms)]
-type DateTime = String;
-
-#[allow(clippy::upper_case_acronyms)]
-type URI = String;
-
-// The paths are relative to the directory where your `Cargo.toml` is located.
-// Both json and the GraphQL schema language are supported as sources for the schema
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "schemas/schema.docs.graphql",
-    query_path = "get_repository_discussions.graphql",
-    response_derives = "Debug, Serialize, Deserialize"
-)]
-pub struct GetRepositoryDiscussions;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    log::set_logger(log::logger()).expect("logger set up failed");
 
-    // 读取配置
+    log::info!("begin");
+
+    // 读取配置构建 reqwest client
     let Config { token, user_agent } = serde_yaml::from_reader(std::fs::File::open("config.yml")?)?;
-
     let auth = format!("bearer {token}");
 
-    // TODO: 这里可以 collect
-    let headers: HeaderMap = HeaderMap::from_iter([
-        (header::AUTHORIZATION, auth.parse()?),
-        (header::USER_AGENT, user_agent.parse()?),
-    ]);
-
     let client = blocking::Client::builder()
-        .default_headers(headers)
-        // 这个设置很重要，似乎不选择协议的话，客户端还是会按默认 http。
+        .default_headers(header::HeaderMap::from_iter([
+            (header::AUTHORIZATION, auth.parse()?),
+            (header::USER_AGENT, user_agent.parse()?),
+        ]))
+        // https_only，似乎不选择协议的话，客户端还是会按默认 http。（不明）
         .https_only(true)
         .build()?;
 
-    let variables = Variables {
-        repo_owner: "mermaid-js".to_string(),
-        repo_name: "mermaid".to_string(),
-        // TODO 还不确定这个缺省该怎么处理
-        query_cursor: Some("Y3Vyc29yOnYyOpK5MjAyMy0wNi0wM1QwMjowOToyMSswODowMM4AUD8C".into()),
-        query_window: Some(3),
+    let parsed_json = query::operate_query(client)?;
+
+    dump_output(&parsed_json, "get_repository_discussions.json")?;
+
+    log::info!("end");
+
+    Ok(())
+}
+
+fn dump_output(parsed_json: &str, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+    use std::{fs, io::Write, path::Path};
+
+    let output_dir = Path::new("output");
+
+    if fs::read_dir(output_dir).is_err() {
+        fs::create_dir(output_dir)?;
     };
 
-    let response_body = post_graphql_blocking::<GetRepositoryDiscussions, _>(
-        &client,
-        "https://api.github.com/graphql",
-        variables,
-    )?;
-
-    let response_data: get_repository_discussions::ResponseData =
-        response_body.data.expect("missing response data");
-
-    let parsed_json = serde_json::to_string(&response_data)?;
-
-    // 把 parsed_json 这个文件导出到 output.json 当中
-    let mut file = std::fs::File::create("output2.json")?;
-    file.write_all(parsed_json.to_string().as_bytes())?;
-
-    println!("finished");
+    fs::File::create(output_dir.join(Path::new(filename)))
+        .expect("文件打开失败")
+        .write_all(parsed_json.to_string().as_bytes())
+        .expect("文件写入失败");
 
     Ok(())
 }
---
+++ src/query.rs
@@ -0,0 +1,43 @@
+use graphql_client::reqwest::post_graphql_blocking;
+use graphql_client::GraphQLQuery;
+use reqwest::blocking;
+
+// TODO: 暂时不知道为什么，但是 https://github.com/graphql-rust/graphql-client/blob/main/examples/github/examples/github.rs 案例中这样写。
+#[allow(clippy::upper_case_acronyms)]
+type DateTime = String;
+
+#[allow(clippy::upper_case_acronyms)]
+type URI = String;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schemas/schema.docs.graphql",
+    query_path = "get_repository_discussions.graphql",
+    response_derives = "Debug, Serialize, Deserialize"
+)]
+// 一个 get_repository_discussions 命名的模块会包含进来。
+pub struct GetRepositoryDiscussions;
+
+pub fn operate_query(client: blocking::Client) -> Result<String, Box<dyn std::error::Error>> {
+    // TODO 经过分析我发觉，Variables 是每个任务都不一样的，在 post_graphql_blocking 的时候其实隐含了类型信息。所以不可以抽象成一组高度类似的函数。
+    // TODO 下一步是实现循环请求，要考虑 github graphql 的请求上限，需要做一点计算。
+
+    let variables = get_repository_discussions::Variables {
+        repo_owner: "mermaid-js".to_string(),
+        repo_name: "mermaid".to_string(),
+        // TODO 还不确定这个缺省该怎么处理
+        query_cursor: Some("Y3Vyc29yOnYyOpK5MjAyMy0wNi0wM1QwMjowOToyMSswODowMM4AUD8C".into()),
+        query_window: Some(3),
+    };
+
+    let response_data = post_graphql_blocking::<GetRepositoryDiscussions, _>(
+        &client,
+        "https://api.github.com/graphql",
+        variables,
+    )
+    .expect("failed to execute query")
+    .data
+    .expect("missing response data");
+
+    Ok(serde_json::to_string(&response_data)?)
+}
